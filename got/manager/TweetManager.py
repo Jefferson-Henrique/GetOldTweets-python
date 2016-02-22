3d23114002b887634ee3d1202d0ef4f904c102b8
Jefferson Henrique <jeffersonpi@hotmail.com>
2016-02-22 16:08:29 -0300

    - Fixed multiple whitespace on tweet

--- got/manager/TweetManager.py
+++ got/manager/TweetManager.py
@@ -29,7 +29,7 @@ class TweetManager:
 				tweet = models.Tweet()
 				
 				usernameTweet = tweetPQ("span.username.js-action-profile-name b").text();
-				txt = re.sub(r"[^\x00-\x7F]", "", tweetPQ("p.js-tweet-text").text()).replace('# ', '#').replace('@ ', '@');
+				txt = re.sub(r"\s+", " ", re.sub(r"[^\x00-\x7F]", "", tweetPQ("p.js-tweet-text").text()).replace('# ', '#').replace('@ ', '@'));
 				retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""));
 				favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""));
 				dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"));
