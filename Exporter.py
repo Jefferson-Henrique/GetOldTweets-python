a023777dcf1e87fd437b8cda9a17d4e729c0508c
Andy Biar <andybiar@gmail.com>
2016-06-11 13:38:37 -0400

    changed -h command to correctly suggest using the filename Exporter.py instead of a broken filename

--- Exporter.py
+++ Exporter.py
@@ -18,13 +18,13 @@ def main(argv):
 
  \nExamples:
  # Example 1 - Get tweets by username [barackobama]
- python Export.py --username 'barackobama' --maxtweets 1\n
+ python Exporter.py --username 'barackobama' --maxtweets 1\n
 
  # Example 2 - Get tweets by query search [europe refugees]
- python Export.py --querysearch 'europe refugees' --maxtweets 1\n
+ python Exporter.py --querysearch 'europe refugees' --maxtweets 1\n
 
  # Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']
- python Export.py --username 'barackobama' --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n"""
+ python Exporter.py --username 'barackobama' --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n"""
 		return
  
 	try:
