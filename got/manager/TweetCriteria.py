e60c3da9c443665a3eef10f38da6b554ad458fcf
Daniel Dandurand <divandy27@gmail.com>
2016-07-20 02:56:28 -0700

    Revert "Added an "allTweets" attribute to TweetCriteria - if allTweets set to "False","
    This reverts commit dde05d3441e7393a6f2a7f4e2f6b9f67fd4e4167.

--- got/manager/TweetCriteria.py
+++ got/manager/TweetCriteria.py
@@ -1,27 +1,24 @@
 class TweetCriteria:
-
-    def __init__(self):
-        self.maxTweets = 0
-
-    def setUsername(self, username):
-        self.username = username
-        return self
-
-    def setSince(self, since):
-        self.since = since
-        return self
-
-    def setUntil(self, until):
-        self.until = until
-        return self
-
-    def setQuerySearch(self, querySearch):
-        self.querySearch = querySearch
-        return self
-
-    def setMaxTweets(self, maxTweets):
-        self.maxTweets = maxTweets
-        return self
-
-    def setAllTweets(self, allTweets=False):
-        self.allTweets = allTweets
+	
+	def __init__(self):
+		self.maxTweets = 0
+		
+	def setUsername(self, username):
+		self.username = username
+		return self
+		
+	def setSince(self, since):
+		self.since = since
+		return self
+	
+	def setUntil(self, until):
+		self.until = until
+		return self
+		
+	def setQuerySearch(self, querySearch):
+		self.querySearch = querySearch
+		return self
+		
+	def setMaxTweets(self, maxTweets):
+		self.maxTweets = maxTweets
+		return self
