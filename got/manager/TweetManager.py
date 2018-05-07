61bd4f47bdc6e2d8bf7dfaff7c03f741fbb8f34f
ekoyudhi <ekoyudhiprastowo@gmail.com>
2018-05-07 14:18:20 +0700

    edit parsing tweet text '#' and '@'

--- got/manager/TweetManager.py
+++ got/manager/TweetManager.py
@@ -40,6 +40,7 @@ class TweetManager:
 				
 				usernameTweet = tweetPQ("span:first.username.u-dir b").text()
 				txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
+				txt = txt.replace('# ','#').replace('@ ','@')
 				retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
 				favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
 				dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"))
