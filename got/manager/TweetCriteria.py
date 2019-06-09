dcbeedb0a34a18d8cc012a85e436922d21bbc8ba
XR213 <51536473+XR213@users.noreply.github.com>
2019-06-09 17:58:11 +0100

    Update TweetCriteria.py

--- got/manager/TweetCriteria.py
+++ got/manager/TweetCriteria.py
@@ -16,7 +16,8 @@ class TweetCriteria:
 		self.until = until
 		return self
 		
-	def setQuerySearch(self, querySearch):
+	def setQuerySearch(self, querySearch):tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
+	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
 		self.querySearch = querySearch
 		return self
 		
